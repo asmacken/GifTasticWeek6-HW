eebf56c30c806859b3a89cf1ca0d7a1618c9515d
Ashley Mackenzie <shannon.ashley.mackenzie@gmail.com>
2018-01-20 13:48:45 -0500

    update

--- javascript/script.js
+++ javascript/script.js
@@ -29,6 +29,53 @@ $("button").on("click", function() {
       musicianDiv.append(musicianImage);
 
       $("#gifs-appear-here").prepend(musicianDiv);
+
+      function renderButtons() {
+
+        $("#musician-view").empty();
+
+        for (var i = 0; i < musicianDiv.length; i++) {
+
+
+        var a = $("<button>");
+        a.addClass("artist");
+        a.attr("data-name", artist[i]);
+        a.text(artist[i]);
+        $("#musician-view").append(a);
+        }
+      }
+
+      $("#add-musician").on("click", function(event) {
+        event.preventDefault();
+
+        var singer = $("#musician-input").val().trim();
+
+        musicianDiv.push(singer);
+
+        renderButtons();
+      });
+
+      renderButtons();
+
+
+
+      // $(".gif").on("click", function() {
+      //   // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+      //   var state = $(this).attr("data-state");
+      //
+      //   // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+      //   // Then, set the image's data-state to animate
+      //   // Else set src to the data-still value
+      //   if (state === "still") {
+      //     $(this).attr("#gifs-appear-here", $(this).attr("data-animate"));
+      //     $(this).attr("data-state", "animate");
+      //   } else {
+      //     $(this).attr("src", $(this).attr("data-still"));
+      //     $(this).attr("data-state", "still");
+      //   }
+      // });
+
+
     }
   });
 });
